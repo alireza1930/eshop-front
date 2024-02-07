a39019efeb7136ce07d30280d8afaeaa243b7d3e
A_Tajik <A_Tajik@ISC.CO.IR>
2024-02-07 14:56:20 +0330

    add home-page, header and footer

--- isc/apps/ngshop/src/app/app.component.ts
+++ isc/apps/ngshop/src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import {Component} from '@angular/core';
+import {RouterModule} from '@angular/router';
+import {NxWelcomeComponent} from './nx-welcome.component';
+import {HeaderComponent} from "./shared/header/header.component";
+import {FooterComponent} from "./shared/footer/footer.component";
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, HeaderComponent, FooterComponent],
   selector: 'isc-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
