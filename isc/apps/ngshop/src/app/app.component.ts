142f3d98b283b3124d76ea50fdcaace9a103b73f
Alireza Tajik <alireza.tajik1988@gmail.com>
2024-02-11 23:48:11 +0330

    add component from library to app

--- isc/apps/ngshop/src/app/app.component.ts
+++ isc/apps/ngshop/src/app/app.component.ts
@@ -3,10 +3,11 @@ import {RouterModule} from '@angular/router';
 import {NxWelcomeComponent} from './nx-welcome.component';
 import {HeaderComponent} from "./shared/header/header.component";
 import {FooterComponent} from "./shared/footer/footer.component";
+import {BannerComponent} from "@isc/ui";
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule, HeaderComponent, FooterComponent],
+  imports: [NxWelcomeComponent, RouterModule, HeaderComponent, FooterComponent, BannerComponent],
   selector: 'isc-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
---
+++ isc/ui/src/lib/banner/banner.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  
+  // eslint-disable-next-line @angular-eslint/component-selector
+  selector: 'ui-banner',
+  standalone: true,
+  imports: [],
+  templateUrl: './banner.component.html',
+  styleUrl: './banner.component.scss'
+})
+export class BannerComponent {
+
+}
